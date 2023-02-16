c88f154aa169ea0afdb22a5a1de4bb74370c8083
Nikita Volynkin <niknorff@gmail.com>
2023-02-16 14:52:59 +0200

    config update

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,4 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+module.exports = {
+	projectId: "d6nqq6",
+	e2e: {},
+};
