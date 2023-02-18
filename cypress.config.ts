00347ccd8d03704a65e553cb798364afcbc8f484
Nikita Volynkin <niknorff@gmail.com>
2023-02-18 13:58:27 +0200

    server adddress change

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,16 @@
 module.exports = {
-  projectId: "d6nqq6",
+	projectId: "d6nqq6",
 
-  e2e: {
-    specPattern: "cypress/e2e/dashboard-tests",
-    baseUrl: "http://localhost:4200/#",
-  },
+	e2e: {
+		specPattern: "cypress/e2e/dashboard-tests",
+		baseUrl: "http://localhost:4200/#",
+	},
 
-  component: {
-    devServer: {
-      framework: "angular",
-      bundler: "webpack",
-    },
-    specPattern: "**/*.cy.ts",
-  },
+	component: {
+		devServer: {
+			framework: "angular",
+			bundler: "webpack",
+		},
+		specPattern: "**/*.cy.ts",
+	},
 };
