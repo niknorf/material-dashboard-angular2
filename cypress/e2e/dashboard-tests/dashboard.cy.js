8691c6d1103666118413c8a98a2e0f93fc5bac5d
Nikita Volynkin <niknorff@gmail.com>
2023-02-18 15:05:32 +0200

    dashboard url update

--- cypress/e2e/dashboard-tests/dashboard.cy.js
+++ cypress/e2e/dashboard-tests/dashboard.cy.js
@@ -1,6 +1,6 @@
 context("Dashboard tests", () => {
 	it("Lets you check a checkbox", () => {
-		cy.visit("/dashboard");
+		cy.visit("#/dashboard");
 		cy.get(".tasks").find("tr").eq(1).find(".form-check").click();
 	});
 });
